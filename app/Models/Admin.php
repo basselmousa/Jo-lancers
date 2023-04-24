f750bdbf981a97961e6348033cd1e34122d5913a
Bassel Mousa <bassel mousa>
2023-04-24 23:49:17 +0300

    adding admin table

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -8,7 +8,8 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class ServiceProvider extends Authenticatable
+class Admin extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+    protected $guarded = [];
 }
--- app/Models/PostSkill.php
+++ app/Models/PostSkill.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class PostSkill extends Model
 {
     use HasFactory;
+    protected $guarded = [];
+
 }
